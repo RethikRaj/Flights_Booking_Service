9c348519d05534ac93470672308cc42ce3fed600
RethikRaj <rethikrajrr@gmail.com>
2025-07-26 08:32:02 +0530

    basic setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { ServerConfig , Logger} = require('./config'); // ./config/index.js == ./config
+const apiRoutes = require('./routers')
+
+const app = express();
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, ()=>{
+    console.log(`Server is running on port ${ServerConfig.PORT}`);
+})
